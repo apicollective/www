4e2b3fec4b974c508c8938d8731027d16e1b79a0
Michael Bryzek <mbryzek@alum.mit.edu>
2017-06-22 16:53:23 -0400

    Update release script

--- release.go
+++ release.go
@@ -10,6 +10,6 @@ func main() {
 	executor = executor.Add("git pull origin master")
 	executor = executor.Add("dev tag")
 	executor = executor.Add("aws s3 sync apibuilder_files s3://www.apibuilder.io/apibuilder_files --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers")
-	executor = executor.Add("aws s3 cp dist/index.html s3://www.apibuilder.io --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers")
+	executor = executor.Add("aws s3 cp index.html s3://www.apibuilder.io --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers")
 	executor.Run()
 }
