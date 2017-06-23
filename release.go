8b422f513ad257587c0731f18e52486a43ca10dd
Michael Bryzek <mbryzek@alum.mit.edu>
2017-06-23 17:37:14 -0400

    Update paths

--- release.go
+++ release.go
@@ -9,7 +9,8 @@ func main() {
 	executor = executor.Add("git checkout master")
 	executor = executor.Add("git pull origin master")
 	executor = executor.Add("dev tag")
-	executor = executor.Add("aws s3 sync apibuilder_files s3://www.apibuilder.io/apibuilder_files --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers")
+	executor = executor.Add("aws s3 sync files s3://www.apibuilder.io/files --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers")
+	executor = executor.Add("aws s3 sync misc s3://www.apibuilder.io/misc --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers")
 	executor = executor.Add("aws s3 cp index.html s3://www.apibuilder.io --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers")
 	executor.Run()
 }
